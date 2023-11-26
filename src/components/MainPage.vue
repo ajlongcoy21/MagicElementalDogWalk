2ebe4e9726d7dd099fb3f40dd567ff5c29450ea9
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-11-25 20:23:47 -0500

    added app logos; removed unused files; created main page

---
+++ src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="image">
+    <img src="@/assets/images/logo_medw_bg_white_lines.png" alt="logo">
+  </div>
+</template>
+
+<style scoped>
+.image {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
