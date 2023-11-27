eb0adb290167cf6b4a6b0d64023deb81d3a47d7a
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-11-26 20:54:22 -0500

    updated logo images for dark and light mode; installed vueUse;

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { usePreferredDark } from '@vueuse/core'
+
+// is user prefers dark theme
+const isDark = usePreferredDark()
 
 </script>
 
 <template>
   <div id="image">
-    <img class="center" src="@/assets/images/logo_medw_bg_white_lines.png" alt="logo">
+    <img class="center" src="@/assets/images/logo_medw_nobg_white_lines.png" alt="logo" v-if="isDark">
+    <img class="center" src="@/assets/images/logo_medw_nobg_black_lines.png" alt="logo" v-if="!isDark">
   </div>
 </template>
 
@@ -17,10 +22,11 @@ div {
 }
 
 img {
+  /* Have image resize based on the view size */
   width: 100%;
   height: auto;
-  /* Magic! */
   max-width: 60vw;
+  /* Center within View */
   position: absolute;
   left: 50%;
   top: 50%;
