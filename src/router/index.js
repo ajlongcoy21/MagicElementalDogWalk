cde370e76eb8c3979aca18b44066e22a3fe5edcf
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-06 21:17:14 -0500

    testing routes;

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  base: './',
+  base: '/MagicElementalDogWalk/',
   history: createWebHistory('/MagicElementalDogWalk/'),
   routes: [
     {
