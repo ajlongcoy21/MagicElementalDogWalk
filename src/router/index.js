4ca44ccc2649e318f671f032c0acfa4b3fb9c061
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-06 21:07:12 -0500

    fix routing;

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  base: '/MagicElementalDogWalk',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
