a7a5f12b2946d2200899cb8e8d9b6a2574f30405
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-06 22:22:44 -0500

    finally fixed routes

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => DogPage
+      component: DogPage
     },
     {
       path: '/shop',
@@ -42,7 +42,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => ShopPage
+      component: ShopPage
     }
   ]
 })
