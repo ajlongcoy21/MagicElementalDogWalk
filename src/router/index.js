e7d71cab739b23d23863b6fa20b5793206a66325
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-06 22:12:23 -0500

    testing routes;

--- src/router/index.js
+++ src/router/index.js
@@ -17,14 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
-    // {
-    //   path: '/character',
-    //   name: 'character',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../components/CharacterPage.vue')
-    // },
+    {
+      path: '/character',
+      name: 'character',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../components/CharacterPage.vue')
+    },
     // {
     //   path: '/dog',
     //   name: 'dog',
