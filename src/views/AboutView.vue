cce694e7ea6db0bbb87290cfad1b976e8259946c
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-09 00:04:51 -0500

    put info on the initial view;

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,22 @@
   <q-layout view="hHh lpR fFf">
 
     <q-page-container>
-      <!-- <RouterView /> -->
+      <q-page class="fixed-top row justify-center items-center">
+        <div class="column justify-center items-center">
+          <p class="row text-bold text-underlined">UNDER CONSTRUCTION</p>
+          <pre class="row">
+                  App: Magic Elemental Dog Walk
+            Designers: Woodland Elementary School - Lego League Students
+                 Team: Hawk Bots
+               Season: 2023/2024
+                 Info: Please click on the tabs below to see the designs
+                       the team has made. The app is still under contruction
+                       and will continue to be built following their desing specs.
+                       This is an initial demo.
+
+          </pre>
+        </div>
+      </q-page>
     </q-page-container>
 
     <q-footer reveal elevated class="bg-warning text-black">
