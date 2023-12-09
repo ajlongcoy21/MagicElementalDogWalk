93e08b08fa623b834ffe786a9d95d5f31a07bf74
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-08 23:32:26 -0500

    added characters;

--- src/components/CharacterPage.vue
+++ src/components/CharacterPage.vue
@@ -1,7 +1,41 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <div>CHARACTER PAGE</div>
+    <q-page-container>
+      <q-page class="fixed-top row justify-center items-center">
+        <q-carousel
+          animated
+          v-model="slide"
+          arrows
+          navigation
+          infinite
+          style='height: auto; width: 70vw;'
+        >
+          <q-carousel-slide :name="1" class="column bg-green">
+            <q-img
+              src="@/assets/images/male.png"
+              spinner-color="white"
+              fit="scale-down"
+            >
+              <div class="absolute-top text-subtitle1 text-weight-bolder text-center">
+                MALE
+              </div>
+            </q-img>
+          </q-carousel-slide>  
+          <q-carousel-slide :name="2" class="column bg-orange">
+            <q-img
+              src="@/assets/images/female.png"
+              spinner-color="white"
+              fit="scale-down"
+            >
+              <div class="absolute-top text-subtitle1 text-weight-bolder text-center">
+                FEMALE
+              </div>
+            </q-img>
+          </q-carousel-slide>  
+        </q-carousel>
+      </q-page>
+    </q-page-container>
 
     <q-footer reveal elevated class="bg-warning text-black">
       <q-tabs align="center">
@@ -14,3 +48,17 @@
     </q-footer>
   </q-layout>
 </template>
+
+<script>
+  import { QImg } from 'quasar';
+import { ref } from 'vue'
+
+  export default {
+    setup() {
+        return {
+            slide: ref(1)
+        };
+    },
+    components: { QImg }
+}
+</script>
