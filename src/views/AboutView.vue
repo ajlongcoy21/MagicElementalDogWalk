f8a797352d51626ab01bcd7b09c5958995e4d63d
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-09 00:29:17 -0500

    final adjustments for tomorrow

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,20 +3,21 @@
 
     <q-page-container>
       <q-page class="fixed-top row justify-center items-center">
-        <div class="column justify-center items-center">
-          <p class="row text-bold text-underlined">UNDER CONSTRUCTION</p>
-          <pre class="row">
-                  App: Magic Elemental Dog Walk
-            Designers: Woodland Elementary School - Lego League Students
-                 Team: Hawk Bots
-               Season: 2023/2024
-                 Info: Please click on the tabs below to see the designs
-                       the team has made. The app is still under contruction
-                       and will continue to be built following their desing specs.
-                       This is an initial demo.
+        <q-card
+          class="my-card text-black bg-warning"
+        >
+          <q-card-section class="text-center">
+            <div class="text-h6">Magic Elemental Dog Walk</div>
+            <div class="text-subtitle2">Woodland Elementary School Hawk Bots (2023/2024)</div>
+          </q-card-section>
 
-          </pre>
-        </div>
+          <q-card-section class="q-pt-none">
+            Please click on the tabs below to see the designs
+                        the team has made. The app is still under contruction
+                        and will continue to be built following their desing specs.
+                        This is an initial demo.
+          </q-card-section>
+        </q-card>
       </q-page>
     </q-page-container>
 
@@ -31,3 +32,9 @@
     </q-footer>
   </q-layout>
 </template>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 400px
+</style>
