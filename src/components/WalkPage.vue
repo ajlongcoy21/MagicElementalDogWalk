ebdfbfa43246f4c1f310ceb28350885bafead1dc
ajlongcoy21 <ajlongcoy21@gmail.com>
2023-12-08 22:57:38 -0500

    added under construction;

--- src/components/WalkPage.vue
+++ src/components/WalkPage.vue
@@ -1,7 +1,14 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
-    <div>WALK PAGE</div>
+    <q-page-container>
+      <q-page class="fixed-top row justify-center items-center">
+        <div class="column justify-center items-center">
+          <q-icon :name="matHandyman" size="300px" class="col">
+          </q-icon>
+          <p class="row">UNDER CONSTRUCTION</p>
+        </div>
+      </q-page>
+    </q-page-container>
 
     <q-footer reveal elevated class="bg-warning text-black">
       <q-tabs align="center">
@@ -14,3 +21,7 @@
     </q-footer>
   </q-layout>
 </template>
+
+<script setup>
+  import { matHandyman } from '@quasar/extras/material-icons'
+</script>
